9d3df2812d0126c2355b5ebb3e19b5c775ef8a7c
Athif Binu <binuathif@gmail.com>
2025-01-06 13:10:44 +0530

    fetch

--- frontent/src/App.jsx
+++ frontent/src/App.jsx
@@ -1,7 +1,20 @@
 // eslint-disable-next-line no-unused-vars
+import { useQuery } from "@tanstack/react-query";
 import React from "react";
+import axios from "axios";
 
 const App = () => {
+  const { data, error, isLoading, isError } = useQuery({
+    queryKey: ["users"],
+    queryFn: async () => {
+      const responsice = await axios("http://localhost:3000/users");
+
+      return responsice.data;
+    },
+  });
+
+  console.log("user data=", data);
+
   return (
     <div>
       <h1>home</h1>
